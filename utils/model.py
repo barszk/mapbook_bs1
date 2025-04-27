8a42e4587c1b8c73dd975faec4dfae289a54d2f7
barszk <u64820@student.wat.edu.pl>
2025-04-27 23:14:46 +0200

    wip: Add interactive map

--- utils/model.py
+++ utils/model.py
@@ -1,7 +1,8 @@
 users: list = [
-    {"name": "Bartosz", "location": "Modliborzyce", "posts": 100},
-    {"name": "Wiktoria", "location": "Chełm", "posts": 6},
-    {"name": "Sabina", "location": "Opole", "posts": 110},
-    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 300},
-    {"name": "Julia", "location": "Żyrardów", "posts": 50},
+    {"name": "Bartosz", "location": "Modliborzyce", "posts": 100,"picture":"https://www.wig.wat.edu.pl/images/aktualnosci/2023/vr_spacer.jpg"},
+    {"name": "Wiktoria", "location": "Chełm", "posts": 6,"picture":"https://www.wig.wat.edu.pl/images/aktualnosci/2023/vr_spacer.jpg"},
+    {"name": "Sabina", "location": "Opole", "posts": 110,"picture":"https://www.wig.wat.edu.pl/images/aktualnosci/2023/vr_spacer.jpg"},
+    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 300,"picture":"https://www.wig.wat.edu.pl/images/aktualnosci/2023/vr_spacer.jpg"},
+    {"name": "Julia", "location": "Żyrardów", "posts": 50,"picture":"https://www.wig.wat.edu.pl/images/aktualnosci/2023/vr_spacer.jpg"},
 ]
+
